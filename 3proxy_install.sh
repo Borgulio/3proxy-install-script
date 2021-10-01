5a8872b97280b74d471a00307dc69e7b6ac79594
Borgulio <30375121+Borgulio@users.noreply.github.com>
2021-10-01 11:44:41 +1000

    Add files via upload

---
+++ 3proxy_install.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+# Скрипт установки 3proxy на операционные системы семейства CentOS
+
+# Этот скрипт будет работать только на CentOS и, возможно, на его
+# производных дистрибутивах
+
+if [[ "$EUID" -ne 0 ]]; then
+	echo "Этот скрипт нужно запускать с правами root"
+	exit 2
+fi
+
+
+
+# Пробуем получить наш IP адрес
+IP=$(wget -4qO- "http://whatismyip.akamai.com/")
+
+
+clear
+echo 'Начинаем установку 3proxy вместе с SecFAll.com'
+echo ""
+# Установка 3proxy
+echo "Несколько вопросов перед началом установки"
+echo "Вы можете оставлять параметры по умолчанию и просто нажимать «Enter», если они вас устраивают."
+echo "Если хотите изменить параметр, то сотрите предлагаемое значение и введите своё"
+echo ""
+echo "Для начала введите IP адрес, на который 3proxy будет принимать подкючения"
+echo "Если автоматически определённый IP адрес правильный, просто нажмите Enter"
+read -p "Определён IP адрес: " -e -i $IP IP
+echo ""
+echo "На какой порт будем принимать подключения HTTP(S) (1080 рекомендуется)?"
+read -p "Порт: " -e -i 1080 PORTHTTP
+echo ""
+echo "На какой порт будем принимать подключения SOCKS5 (1081 рекомендуется)?"
+read -p "Порт: " -e -i 1081 PORTSOCKS
+echo ""
+echo "Имя пользователя для авторизации на прокси"
+read -p "Username: " -e -i user001 BUSERNAME
+echo ""
+echo "Пароль пользователя для авторизации на прокси"
+read -p "Password: " -e -i password BPASSWORD
+echo ""
+echo "Отлично. Сейчас обновим сервер и выполним установку 3proxy."
+read -n1 -r -p "Нажмите любую кнопку для продолжения..."
+yum install epel-release -y
+yum update -y
+yum upgrade -y
+yum install wget zip unzip -y
+yum -y install gcc
+cd /tmp/
+#wget https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz
+wget https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz
+tar -xvzf 0.9.3.tar.gz
+cd 3proxy-0.9.3
+make -f Makefile.Linux
+#mkdir -p /opt/3proxy/bin
+mkdir /etc/3proxy
+#touch /opt/3proxy/3proxy.pid
+#touch /etc/3proxy/3proxy.pid
+#cp ./src/3proxy /opt/3proxy/bin
+cp bin/3proxy /usr/bin/
+#cp ./cfg/3proxy.cfg.sample /opt/3proxy/3proxy.cfg
+touch /etc/3proxy/3proxy.cfg
+#Делаем скрипт управления службой 3proxy
+echo '[Unit]
+Description=3proxy Proxy Server
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/3proxy /etc/3proxy/3proxy.cfg
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/3proxy.service
+
+#Делаем конфиг 3proxy
+echo 'daemon' > /etc/3proxy/3proxy.cfg
+echo 'nserver 8.8.8.8' >> /etc/3proxy/3proxy.cfg
+echo 'nserver 8.8.4.4' >> /etc/3proxy/3proxy.cfg
+echo 'nscache 65536' >> /etc/3proxy/3proxy.cfg
+
+echo '' >> /etc/3proxy/3proxy.cfg
+
+echo 'timeouts 1 5 30 60 180 1800 15 60' >> /etc/3proxy/3proxy.cfg
+echo "users $BUSERNAME:CL:$BPASSWORD" >> /etc/3proxy/3proxy.cfg
+echo 'log /dev/null' >> /etc/3proxy/3proxy.cfg
+
+echo '' >> /etc/3proxy/3proxy.cfg
+
+echo "external $IP" >> /etc/3proxy/3proxy.cfg
+echo "" >> /etc/3proxy/3proxy.cfg
+
+echo '#HTTP(S)' >> /etc/3proxy/3proxy.cfg
+echo 'auth strong' >> /etc/3proxy/3proxy.cfg
+echo 'flush' >> /etc/3proxy/3proxy.cfg
+echo 'allow *' >> /etc/3proxy/3proxy.cfg
+echo 'maxconn 64' >> /etc/3proxy/3proxy.cfg
+echo "proxy -n -p$PORTHTTP" >> /etc/3proxy/3proxy.cfg
+
+echo "" >> /etc/3proxy/3proxy.cfg
+
+echo '#SOCKS5' >> /etc/3proxy/3proxy.cfg
+echo 'auth strong' >> /etc/3proxy/3proxy.cfg
+echo 'flush' >> /etc/3proxy/3proxy.cfg
+echo 'allow *' >> /etc/3proxy/3proxy.cfg
+echo 'maxconn 64' >> /etc/3proxy/3proxy.cfg
+echo "socks -p$PORTSOCKS" >> /etc/3proxy/3proxy.cfg
+
+/usr/bin/killall 3proxy
+systemctl start 3proxy
+systemctl enable 3proxy
+
+echo "3proxy установлен и запущен"
+read -n1 -r -p "Нажмите любую кнопку для продолжения..."
+
+
